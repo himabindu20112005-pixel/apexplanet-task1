1363eb544d0443bee46f905fb880ddbf4be51135
Himabindu <himabindu20112005@gmail.com>
2026-05-31 15:23:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function showMessage() {
+    alert("Welcome to CodeMaster Academy!");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const themeBtn = document.getElementById("themeBtn");
+
+    themeBtn.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")){
+            themeBtn.innerHTML = "☀️";
+        } else {
+            themeBtn.innerHTML = "🌙";
+        }
+
+    });
+
+});
